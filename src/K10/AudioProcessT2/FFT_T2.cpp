ace568d3bd03174bbfa91c54ffea09296180fab8
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2024-09-02 09:07:11 +0900

    Update FFT_T2.cpp

--- src/K10/AudioProcessT2/FFT_T2.cpp
+++ src/K10/AudioProcessT2/FFT_T2.cpp
@@ -8,10 +8,12 @@ FFT_T2::FFT_T2(int window_size) : FFT(), m_fft_size(1), m_window_size(window_siz
         m_fft_size *= 2;
     }
 
-    vReal = static_cast<float *>(malloc(sizeof(float) * m_fft_size));
+    m_fft_input = static_cast<float *>(malloc(sizeof(float) * m_fft_size));
     vImag = static_cast<float *>(malloc(sizeof(float) * m_fft_size));
+    m_fft_output = static_cast<float *>(malloc(sizeof(float) * m_fft_size / 2)); // FFT 출력 버퍼 할당
+
     for (int i = 0; i < m_fft_size; i++) {
-        vReal[i] = 0;
+        m_fft_input[i] = 0;
         vImag[i] = 0; // 허수 부분을 0으로 초기화
     }
 
@@ -21,22 +23,27 @@ FFT_T2::FFT_T2(int window_size) : FFT(), m_fft_size(1), m_window_size(window_siz
 void FFT_T2::update(int16_t *samples) {
     int offset = (m_fft_size - m_window_size) / 2;
     for (int i = 0; i < m_window_size; i++) {
-        vReal[offset + i] = samples[i] / 30.0f;
+        m_fft_input[offset + i] = samples[i] / 30.0f;
         vImag[offset + i] = 0; // 허수 부분은 여전히 0
     }
 
-    // arduinoFFT의 Windowing 함수로 Hamming 윈도우 적용
-    FFT.windowing(vReal, m_fft_size, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    // arduinoFFT의 windowing 함수로 Hamming 윈도우 적용
+    FFT.windowing(m_fft_input, m_fft_size, FFT_WIN_TYP_HAMMING);
 
     // FFT 계산 수행
-    FFT.compute(vReal, vImag, m_fft_size, FFT_FORWARD);
+    FFT.compute(m_fft_input, vImag, m_fft_size);
 
     // 복소수 결과를 크기로 변환
-    FFT.complexToMagnitude(vReal, vImag, m_fft_size);
+    FFT.complexToMagnitude(m_fft_input, vImag, m_fft_size);
+
+    // m_fft_output에 FFT 결과 저장
+    for (int i = 0; i < m_fft_size / 2; i++) {
+        m_fft_output[i] = m_fft_input[i];
+    }
 
     // 에너지 계산
     for (int i = 0; i < m_window_size / 4; i++) {
-        m_energy[i] = vReal[i];
+        m_energy[i] = m_fft_output[i];
     }
 }
 
