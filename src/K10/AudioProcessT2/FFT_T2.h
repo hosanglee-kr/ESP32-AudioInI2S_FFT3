07f0832ea0b8e05cb01f70ae3f2746a1ea0d8b87
hslee <50106977+hosanglee-kr@users.noreply.github.com>
2024-09-02 09:06:15 +0900

    Update FFT_T2.h

--- src/K10/AudioProcessT2/FFT_T2.h
+++ src/K10/AudioProcessT2/FFT_T2.h
@@ -8,15 +8,15 @@ class FFT_T2 {
 private:
     int m_fft_size;
     int m_window_size;
-    
-    float *vReal;    // FFT 입력 데이터의 실수 부분
-    float *vImag;    // FFT 입력 데이터의 허수 부분
-    ArduinoFFT<float> FFT ; //= ArduinoFFT<float>(vReal, vImag, samples, samplingFrequency);;  // arduinoFFT 객체 추가
+    arduinoFFT<float> FFT;  // arduinoFFT<float> 객체 추가
+    float *m_fft_input;     // FFT 입력 데이터의 실수 부분
+    float *vImag;           // FFT 입력 데이터의 허수 부분
+    float *m_fft_output;    // FFT 결과 데이터를 저장하는 변수 (private)
 
 public:
-    float *m_fft_input;     // FFT 입력 데이터의 실수 부분
     float *m_energy;
 
     FFT_T2(int window_size);
     void update(int16_t *samples);
+
 };
